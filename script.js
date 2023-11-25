a07297264c83669365334ad9569a29ef4b729ed5
yovanessaruiz17 <114221797+yovanessaruiz17@users.noreply.github.com>
2023-11-24 21:23:57 -0500

    lista y chat

---
+++ script.js
@@ -0,0 +1,22 @@
+function openChat(employeeName) {
+    document.getElementById('chatTitle').textContent = 'Chat con ' + employeeName;
+    document.getElementById('chatPopup').style.display = 'block';
+}
+
+function closeChat() {
+    document.getElementById('chatPopup').style.display = 'none';
+}
+
+function sendMessage() {
+    const messageInput = document.getElementById('messageInput');
+    const message = messageInput.value;
+
+    if (message.trim() !== '') {
+        const chatMessages = document.getElementById('chatMessages');
+        const newMessage = document.createElement('p');
+        newMessage.textContent = 'Cliente: ' + message;
+        chatMessages.appendChild(newMessage);
+
+        messageInput.value = '';
+    }
+}
